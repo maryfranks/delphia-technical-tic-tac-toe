3ce06ef41cf285e9b79c731fd921b7d50f1bad05
Mary Franks <maryfranks@gmail.com>
2022-05-11 12:39:28 -0400

    add some test skeletons and clean up test file

--- src/tic-tac-toe.test.ts
+++ src/tic-tac-toe.test.ts
@@ -1,25 +1,57 @@
 import { TicTacToeGame } from './tic-tac-toe' 
 
 describe("Class: TicTacToeGame", () => {
-  describe("Instance Method: displayBoard", () => {
-    it("displays human readable gameBoard", () => {
+  describe("Instance Method: play", () => {
+    it("welcomes players at game start", () => {
+
       // arrange
       const newGame = new TicTacToeGame()
       jest.spyOn(console, "log")
+    
+      // act
+      newGame.play()
+
+      // assert
+      expect(console.log).toHaveBeenCalledWith("Welcome!");
+    })
+  })
+  describe("Instance Method: displayBoard", () => {
+    it("displays human readable gameBoard", () => {
+
+      // arrange
+      const newGame = new TicTacToeGame()
+      jest.spyOn(console, "log")
+
       // act
       newGame._displayBoard()
+
       // assert
       // @ts-ignore:next
       expect(console.log.mock.calls[0][0]).toMatchSnapshot()
     })
   })
+  describe("Instance Method: takeTurn", () => {
+    it("should parse player name", () => {
+      // arrange
+      // act
+      // assert
+    })
+    it("should update the gameboard with a marker", () => {
+      // arrange
+      // act
+      // assert
+    })
+  })
+  describe("Instance Method: hasWinner", () => {
+    it("should continue the game if no one has won", () => {
+      // arrange
+      // act
+      // assert
+    })
+    it("should correctly evaluate win condition(s)", () => { // this is probably more than one test
+      // arrange
+      // act
+      // assert
+    })
+  })
 })
-
-test('test', () => {
-  const newGame = new TicTacToeGame()
-
-  jest.spyOn(console, "log")
-
-  newGame.play()
-  expect(console.log).toHaveBeenCalledWith("Welcome!");
-});
